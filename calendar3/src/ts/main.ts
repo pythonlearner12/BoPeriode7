aa798bd15d8440ee8f70fb4d6157595e40be0b46
pythonlearner12 <fhh1914@gmail.com>
2025-02-13 14:44:06 +0100

    minor changes

--- calendar3/src/ts/main.ts
+++ calendar3/src/ts/main.ts
@@ -6,29 +6,24 @@ let dateElement = document.getElementById("dates") as HTMLElement;
 let monthYearTag = document.getElementById("monthYear") as HTMLElement
 
 function calendar(nextMonth: number) {
-
-
     month += nextMonth
     let firstDay = new Date(date.getFullYear(), month + 0, 1).getDay()
     let monthYear = new Date(year, month, date.getDate());
-
 
     monthYearTag.innerText = `${monthYear.toLocaleString('default', { month: 'long' })} ${monthYear.getFullYear()}`
     dateElement.innerHTML = '';
     for (let i = 0; i < lastdate; i++) {
         if (i == 0) {
-            dateElement.innerHTML +=
-                `
-            <button class="col-start-${firstDay} cursor-pointer aspect-square text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
-                <time datetime="2019-02-01">${i + 1}</time>
-            </button>
+            dateElement.innerHTML +=`
+                <button style="grid-column-start: ${firstDay}" class="cursor-pointer aspect-square text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
+                    <time datetime="2019-02-01">${i + 1}</time>
+                </button>
             `;
         } else {
-            dateElement.innerHTML +=
-                `
-            <button class="cursor-pointer text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
-                <time class="aspect-square" datetime="2019-02-01">${i + 1}</time>
-            </button>
+            dateElement.innerHTML +=`
+                <button class="cursor-pointer text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
+                    <time class="aspect-square" datetime="2019-02-01">${i + 1}</time>
+                </button>
             `;
         }
     }
@@ -44,43 +39,3 @@ nextMonth.addEventListener("click", () => {
 prevMonth.addEventListener("click", () => {
     calendar(-1);
 })
-
-
-
-
-
-
-
-
-
-// function monthYear() {
-//     let selected_month = new Date().getMonth()
-//     let selected_year = new Date().getFullYear()
-//     console.log(selected_month)
-//     console.log(selected_year)
-// }
-// monthYear()
-
-
-
-// const month = new Date().toLocaleString('default', { month: 'long' });
-// console.log(month)
-
-// const year = new Date().toLocaleString(undefined, {
-//     year: "numeric",
-//     month: "long",
-//     day: "numeric",
-//     hour: "numeric",
-//     minute: "numeric",
-//     second: "numeric"
-// })
-// console.log(year);
-
-
-
-// Get the first day of the month
-// let dayone = new Date(year, month, 1).getDay();
-
-// Get the last date of the month
-
-// Get the day of the last date of the month
