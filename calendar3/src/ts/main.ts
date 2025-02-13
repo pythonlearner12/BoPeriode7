55b43eeada77a42e4b1966ca59c8f3cd556159d8
pythonlearner12 <fhh1914@gmail.com>
2025-02-13 17:45:38 +0100

    minor changes

--- calendar3/src/ts/main.ts
+++ calendar3/src/ts/main.ts
@@ -1,37 +1,46 @@
 let date = new Date();
+let monthOffsetvar = date.getMonth();
 let year = date.getFullYear();
-let month = date.getMonth();
-let lastdate = new Date(year, month + 1, 0).getDate();
-let dateElement = document.getElementById("dates") as HTMLElement;
-let monthYearTag = document.getElementById("monthYear") as HTMLElement
 
-function calendar(nextMonth: number) {
-    month += nextMonth
-    let firstDay = new Date(date.getFullYear(), month + 0, 1).getDay()
-    let monthYear = new Date(year, month, date.getDate());
+function first_day() {
+    const newDate = new Date();
+    newDate.setMonth(monthOffsetvar);
+    newDate.setDate(1);
+    const firstDayVar = newDate.getDay();
+    const adjustedDay = (firstDayVar === 0) ? 6 : firstDayVar - 1;
+
+    return adjustedDay;
+}
+
+
+const dateElement = document.getElementById("dates") as HTMLElement;
+const monthYearTag = document.getElementById("monthYear") as HTMLElement
+function calendar(monthOffset: number) {
+    monthOffsetvar += monthOffset
+    let firstDay = first_day();
+    let monthYear = new Date(year, monthOffsetvar);
+    let lastdate = new Date(year, monthOffsetvar + 1, 0).getDate();
 
     monthYearTag.innerText = `${monthYear.toLocaleString('default', { month: 'long' })} ${monthYear.getFullYear()}`
-    dateElement.innerHTML = '';
-    for (let i = 0; i < lastdate; i++) {
-        if (i == 0) {
-            dateElement.innerHTML +=`
-                <button style="grid-column-start: ${firstDay}" class="cursor-pointer aspect-square text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
-                    <time datetime="2019-02-01">${i + 1}</time>
-                </button>
-            `;
-        } else {
-            dateElement.innerHTML +=`
-                <button class="cursor-pointer text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
-                    <time class="aspect-square" datetime="2019-02-01">${i + 1}</time>
+    dateElement.innerHTML = Array.from({ length: lastdate }, (_, i) => {
+        if (i === 0) {
+            return `
+                <button style="grid-column-start: ${firstDay+1}" class="cursor-pointer aspect-square text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
+                    <time>${i + 1}</time>
                 </button>
             `;
         }
-    }
+        return `
+            <button class="cursor-pointer text-base p-5 rounded-[100%] bg-inherit text-slate-600 border-none hover:bg-slate-100 hover:text-slate-600 focus:bg-slate-100 focus:text-slate-600">
+                <time class="aspect-square">${i + 1}</time>
+            </button>
+        `;
+    }).join('');
 }
 calendar(0);
 
-let nextMonth = document.getElementById("nextMonth") as HTMLElement;
-let prevMonth = document.getElementById("prevMonth") as HTMLElement;
+const nextMonth = document.getElementById("nextMonth") as HTMLElement;
+const prevMonth = document.getElementById("prevMonth") as HTMLElement;
 
 nextMonth.addEventListener("click", () => {
     calendar(+1);
@@ -39,3 +48,4 @@ nextMonth.addEventListener("click", () => {
 prevMonth.addEventListener("click", () => {
     calendar(-1);
 })
+
